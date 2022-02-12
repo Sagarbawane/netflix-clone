296e7f19ab4deb57bc40bdffeeb2051c85aa6ea7
sagar bawane <sagar@iamclearmind.com>
2022-02-13 00:29:31 +0530

    changes

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -19,17 +19,19 @@ function Nav() {
   
   
 return (
-    <div className={`nav ${show && "nav_black"}`}>
-      <img
-        className="nav_logo"
-        src="https://www.freepnglogos.com/uploads/netflix-logo-text-emblem-31.png"
-      />
-      <img
-        className="nav_avatar"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVWGKNk39rCbUMmHEV3uqusN3WZ4LjYZbhrZfNGJ-85ilFgp095yz1AC5Rg7XRsLXE1eQ&usqp=CAU"
-      />
-    </div>
-  );
+  <div className={`nav ${show && "nav_black"}`}>
+    <img
+      alt="logo"
+      className="nav_logo"
+      src="https://www.freepnglogos.com/uploads/netflix-logo-text-emblem-31.png"
+    />
+    <img
+      alt="logo"
+      className="nav_avatar"
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVWGKNk39rCbUMmHEV3uqusN3WZ4LjYZbhrZfNGJ-85ilFgp095yz1AC5Rg7XRsLXE1eQ&usqp=CAU"
+    />
+  </div>
+);
 }
 
 export default Nav;
